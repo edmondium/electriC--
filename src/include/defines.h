00ac97a5f8c001f4110e192b8efe961c04d79b9a
edmondium <nicko@uin-suska.ac.id>
2022-02-10 12:26:17 +0100

    migrating c to c++ in cons source

---
+++ src/include/defines.h
@@ -0,0 +1,41 @@
+/* src/include/defines.h.  Generated automatically by configure.  */
+#define STDC_HEADERS 1
+
+#define HAVE_FTIME 1
+#define HAVE_GETCWD 1
+#define HAVE_GETHOSTID 1
+#define HAVE_GETPAGESIZE 1
+#define HAVE_GETTIMEOFDAY 1
+#define HAVE_GETWD 1
+#define HAVE_PTHREAD 1
+#define HAVE_QSORT 1
+#define HAVE_SCANDIR 1
+#define HAVE_SETLOCALE 1
+#define HAVE_STRCHR 1
+#define HAVE_VALLOC 1
+#define HAVE_VSNPRINTF 1
+
+#define HAVE_POSIX_READDIR_R 1
+
+/* #undef HAVE_DB_H */
+#define HAVE_DIRENT_H 1
+#define HAVE_FCNTL_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_LOCALE_H 1
+/* #undef HAVE_NDIR_H */
+#define HAVE_SGTTY_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRINGS_H 1
+/* #undef HAVE_SYS_DIR_H */
+/* #undef HAVE_SYS_NDIR_H */
+/* #undef HAVE_SYS_INT_TYPES_H */
+#define HAVE_SYS_TIME_H 1
+#define HAVE_SYS_TIMEB_H 1
+/* #undef HAVE_SYS_TYPES_H */
+#define HAVE_SYS_WAIT_H 1
+#define HAVE_TERMIO_H 1
+#define HAVE_TERMIOS_H 1
+#define HAVE_UNISTD_H 1
+/* #undef HAVE_VFORK_H */
+#define RETSIGTYPE        void
